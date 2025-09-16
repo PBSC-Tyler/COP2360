ae746f69490a6e151de5a8633e557fc21d7e911d
PBSC-Tyler <Tyler.mcdonald@my.pbsc.edu>
2025-09-16 12:54:13 -0400

    Update Panda.cs

--- Panda.cs
+++ Panda.cs
@@ -3,23 +3,18 @@ using System;
 public class Panda
 {
     private string name;
-      public Panda(string n)
+
+    public Panda(string n)
     {
         name = n;
-        Console.WriteLine($"A new panda named {name} has been created!");      
+        Console.WriteLine($"A new panda named {name} has been created!");
     }
 }
 
-Panda p = new Panda ("Petey");   // Call constructor
-
-public class Panda
+class Program
 {
-  string name;                   // Define field
-  public Panda (string n)        // Define constructor
-  {
-    name = n;                    // Initialization code (set up field)
-  }
+    static void Main(string[] args)
+    {
+        Panda po = new Panda("Po");
+    }
 }
-Constructors - Overloading
-
-// You can also overload constructors.
